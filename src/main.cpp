018fa74cbc81f124d0accf9bc08814210e1ccdae
waya0125 <waya0125@waya0125.com>
2023-09-08 15:05:40 +0900

    コード整理前だが期限の関係で一度コミット。

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,50 @@
 //## 環境に応じて設定
 #define WIDTH 1280
 #define HEIGHT 720
-#define SPEED 1 // 移動速度 60Hz=8, 300Hz=1
+#define SPEED_DEFAULT 8 // 移動速度 60Hz=8, 300Hz=1
+#define SPEED_WIRE 4.0f // ワイヤーの移動速度 60Hz=4.0f, 300Hz=0.5f
 using namespace std;
+
+//## 構造体親元宣言
+void calc();     // 計算関数
+void keyInput(); // キー入力管理
 
 //## 型宣言
 // 色の設定
-unsigned int armBody = GetColor(255, 255, 0); // アーム本体（黄色）
-unsigned int wire = GetColor(255, 255, 255);  // ワイヤー（白）
-unsigned int magnet = GetColor(255, 0, 0);    // 磁石（赤）
+const unsigned int colorYellow = GetColor(255, 255, 0);   // アーム本体（黄色）
+const unsigned int colorWhite  = GetColor(255, 255, 255); // ワイヤー（白）
+const unsigned int colorRed    = GetColor(255, 0, 0);     // 磁石（赤）
+
+// その他の変数
+int         intGoalCount            = 0;      // ゴールカウント
+float       floatStaticWireLength   = 100.0f; // ワイヤー長格納
+float       floatWireLength         = 0.0f;   // ワイヤーの長さ格納
+const int   intArmHeight            = 30;     // アーム本体の高さ
+const int   intItemSize             = 50;     // アイテムサイズ
+const float floatGravityAttenuation = 0.99f;  // 重力ベクター減衰率
+const float floatWireMaxLength      = HEIGHT; // ワイヤー最大長
 
 // Vector2構造体の宣言
-Vector2 armPosA, armPosB, armMove;
-Vector2 magnetPos, magnetPendulum, magnetMove, magnetMovePos;
-Vector2 gravity;
+Vector2 vectorArmPos    = Vector2(WIDTH / 2, 20); // アーム本体の座標
+Vector2 vectorGravity   = Vector2(0, 0.2f);       // 重力のベクトル
+Vector2 vectorItemPos   = Vector2(0, 0);          // アイテムの座標
+Vector2 vectorMagnetPos = Vector2(WIDTH / 2, 50); // 磁石の座標
 
-void keyInput();
+// Vector2 運動・相対・座標等格納
+Vector2 vectorPendulumPos    = Vector2(0, 0); // 振り子（マグネット）の座標
+Vector2 vectorPendulumMotion = Vector2(0, 0); // 振り子（マグネット）の運動ベクトル
+Vector2 vectorWireRelative   = Vector2(0, 0); // ワイヤーの相対ベクトル
 
 // プログラムの最初はWinMainで始める
-[[maybe_unused]] int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     //##### 初期設定 #####//
     ChangeWindowMode(TRUE); // ウィンドウモード設定
     SetGraphMode(WIDTH, HEIGHT, 32); // 画面サイズ設定
     SetWaitVSyncFlag(TRUE); // 垂直同期を有効にする
 
-    // Vector2構造体の初期設定
-    armPosA = { WIDTH / 2, 20 };           // アーム本体の座標
-    armPosB = { WIDTH, 50 };               // アーム本体の座標
-    armMove = { SPEED, SPEED };            // アームの移動量
-    magnetPos = { WIDTH / 2, HEIGHT / 2 }; // 磁石の座標
-    magnetPendulum = { 0, 0 };             // マグネットの遠心力
-    magnetMove = { SPEED, SPEED };         // マグネットの移動量
+    // アイテムの座標をランダムに設定する
+    vectorItemPos.x = GetRand(WIDTH - intItemSize);  // 0 ? (WIDTH - intItemSize) の範囲の乱数を取得
+    vectorItemPos.y = GetRand(HEIGHT - intItemSize); // 0 ? (HEIGHT - intItemSize) の範囲の乱数を取得
 
     // DXライブラリ初期化処理
     if (DxLib_Init() == -1) {
@@ -47,32 +61,55 @@ void keyInput();
     while (ProcessMessage() == 0) {
         ClearDrawScreen(); // 画面の更新
 
-        // ふりこの運動ベクタに重力を加算
-        magnetPendulum = addVector(magnetPendulum, gravity);
+        // ゴール回数が5回未満ならContinue
+        if (intGoalCount < 5) {
+            // いずれかのキー入力があった場合
+            keyInput();
 
-        // ふりこの移動地点を計算する
-        magnetMovePos = addVector(magnetPos, magnetPendulum);
+            // 運動ベクタ実験関数
+            calc ();
 
-        // ワイヤーの視点から移動地点までの相対ベクタを取得してその距離を測る
-        float wireLength = getDistance(magnetMovePos, armPosA);
+            // アーム
+            DrawBox(
+                    vectorArmPos.x, vectorArmPos.y,
+                    WIDTH, vectorArmPos.y + intArmHeight,
+                    colorYellow,
+                    TRUE
+            );
+            // ワイヤー
+            DrawLine(
+                    vectorArmPos.x, vectorArmPos.y + 30,
+                    vectorMagnetPos.x, vectorMagnetPos.y,
+                    colorWhite
+            );
+            // 磁石
+            DrawCircle(
+                    vectorMagnetPos.x, vectorMagnetPos.y,
+                    10,
+                    colorRed,
+                    TRUE
+            );
+            // アイテム
+            DrawBox(
+                    vectorItemPos.x, vectorItemPos.y,
+                    vectorItemPos.x + intItemSize, vectorItemPos.y + intItemSize,
+                    colorWhite,
+                    TRUE
+            );
 
-        // ふりこの移動地点をワイヤーが届く位置に補正する
-        // 正規化する 始点（アーム本体側のワイヤ起点）→移動地点（磁石の遠心力）のベクトルを正規化する
-        // あとで書く
+            // ゴールカウントを描画
+            printfDx ("ゴールカウント = %d\n", intGoalCount);
+        }
+        // ゴール回数が5回以上ならゲームクリアを描画してBreak
+        else {
+            DrawString(WIDTH / 2, HEIGHT / 2, "ゲームクリア", colorWhite);
+            DrawString(WIDTH / 2, HEIGHT / 2 + 20, "Enterキーで終了", colorWhite);
 
-
-
-        // アーム本体の描画
-        DrawBox(armPosB.x, armPosB.y, armPosA.x, armPosA.y, armBody, TRUE);
-
-        // ワイヤーの描画 +30はアーム本体の高さ
-        DrawLine(armPosA.x, armPosA.y + 30, magnetPos.x, magnetPos.y, wire);
-
-        // 磁石の描画
-        DrawCircle(magnetPos.x, magnetPos.y, 10, magnet, TRUE);
-
-        // いずれかのキー入力があった場合
-        keyInput();
+            // エンターキーが押されたらBreak
+            if (CheckHitKey(KEY_INPUT_RETURN)) {
+                return 0;
+            }
+        }
 
         // 画面の更新（必須）
         ScreenFlip(); // 画面を反転させる処理
@@ -83,31 +120,104 @@ void keyInput();
     return 0;    // ソフトを正常終了
 }
 
+void calc() {
+    // 運動ベクトルに重力を加算
+    vectorPendulumMotion = {
+        vectorPendulumMotion.x + vectorGravity.x, // 運動ベクトルXに重力ベクトルXを加算
+        vectorPendulumMotion.y + vectorGravity.y  // 運動ベクトルYに重力ベクトルYを加算
+    };
+
+    // 振り子の座標計算
+    vectorPendulumPos = {
+        vectorMagnetPos.x + vectorPendulumMotion.x, // 磁石の座標Xに運動ベクトルXを加算
+        vectorMagnetPos.y + vectorPendulumMotion.y  // 磁石の座標Yに運動ベクトルYを加算
+    };
+
+    // ワイヤーの相対ベクトルを計算
+    vectorWireRelative = {
+        vectorPendulumPos.x - vectorArmPos.x, // 振り子の座標Xからアーム本体の座標Xを引く
+        vectorPendulumPos.y - vectorArmPos.y  // 振り子の座標Yからアーム本体の座標Yを引く
+    };
+
+    // ワイヤーの始点から移動地点までの距離を相対ベクトル(sqrt)にする
+    floatWireLength = sqrt((vectorWireRelative.x * vectorWireRelative.x) + (vectorWireRelative.y * vectorWireRelative.y));
+
+    // ワイヤーの長さを計算して最大長を超えた場合
+    if (floatStaticWireLength < floatWireLength) {
+        // ワイヤーの長さ（Relative）をワイヤーの長さで割って正規化
+        Vector2 normTmp = {
+            vectorWireRelative.x / floatWireLength, // ワイヤーの長さXをワイヤーの長さで割る
+            vectorWireRelative.y / floatWireLength  // ワイヤーの長さYをワイヤーの長さで割る
+        };
+        // 正規化したベクトルにワイヤーの長さをかける
+        Vector2 scaleTmp = {
+            normTmp.x * floatStaticWireLength, // 正規化したベクトルXにワイヤーの長さをかける
+            normTmp.y * floatStaticWireLength  // 正規化したベクトルYにワイヤーの長さをかける
+        };
+        // ワイヤーが届く位置に戻す
+        vectorPendulumPos = {
+            vectorArmPos.x + scaleTmp.x, // アーム本体の座標Xにワイヤーの長さXを加算
+            vectorArmPos.y + scaleTmp.y  // アーム本体の座標Yにワイヤーの長さYを加算
+        };
+
+        // 位置を戻したら運動ベクタに補正をかける
+        vectorPendulumMotion = {
+            vectorPendulumPos.x - vectorMagnetPos.x, // 振り子の座標Xから磁石の座標Xを引く
+            vectorPendulumPos.y - vectorMagnetPos.y  // 振り子の座標Yから磁石の座標Yを引く
+        };
+
+        // 運動ベクトルを減衰させる
+        vectorPendulumMotion = {
+            vectorPendulumMotion.x *= floatGravityAttenuation, // 運動ベクトルXに重力ベクトルXを加算
+            vectorPendulumMotion.y *= floatGravityAttenuation  // 運動ベクトルYに重力ベクトルYを加算
+        };
+    }
+
+    // マグネットの座標を更新
+    vectorMagnetPos = vectorPendulumPos;
+
+    // アイテムの描画座標内にマグネットがある場合
+    if (vectorMagnetPos.x >= vectorItemPos.x && vectorMagnetPos.x <= vectorItemPos.x + intItemSize &&
+        vectorMagnetPos.y >= vectorItemPos.y && vectorMagnetPos.y <= vectorItemPos.y + intItemSize) {
+        // アイテムの座標をランダムに設定する
+        vectorItemPos.x = GetRand(WIDTH - intItemSize);  // 0 ? (WIDTH - intItemSize) の範囲の乱数を取得
+        vectorItemPos.y = GetRand(HEIGHT - intItemSize); // 0 ? (HEIGHT - intItemSize) の範囲の乱数を取得
+
+        // ゴールカウントを増やす
+        intGoalCount++;
+    }
+}
+
 // キー入力管理
 void keyInput() {
     // キー入力の取得
-    char key[256];
-    GetHitKeyStateAll(key);
+    char key[256];                         // キー入力を格納する配列
+    GetHitKeyStateAll(key); // 全てのキーの入力状態を得る
 
     // キー入力があった場合
-    if (key[KEY_INPUT_UP] == 1 || key[KEY_INPUT_W] == 1) {
-        // アーム本体を上に移動させる
-        armPosA.y -= SPEED;
-        armPosB.y -= SPEED;
+    // 十字キーとWASDで機能を分離
+    // キー"↑"が押された場合 && アーム座標Yが0より大きい場合
+    if (key[KEY_INPUT_UP] == 1 && vectorArmPos.y > 0) {
+        vectorArmPos.y -= SPEED_DEFAULT; // アーム本体を上に移動させる
     }
-    if (key[KEY_INPUT_DOWN] == 1 || key[KEY_INPUT_S] == 1) {
-        // アーム本体を下に移動させる
-        armPosA.y += SPEED;
-        armPosB.y += SPEED;
+    // キー"↓"が押された場合 && アーム座標Yが画面の高さ + ワイヤー長より小さい場合
+    if (key[KEY_INPUT_DOWN] == 1 && vectorArmPos.y < (HEIGHT - floatStaticWireLength)) {
+        vectorArmPos.y += SPEED_DEFAULT; // アーム本体を下に移動させる
     }
-    if (key[KEY_INPUT_LEFT] == 1 || key[KEY_INPUT_A] == 1) {
-        // アーム本体を左に移動させる
-        armPosA.x -= SPEED;
-        magnetPos.x -= SPEED;
+    // キー"←"が押された場合 && アーム座標Xが0より大きい場合
+    if (key[KEY_INPUT_LEFT] == 1 && vectorArmPos.x > 0) {
+        vectorArmPos.x -= SPEED_DEFAULT; // アーム本体を左に移動させる
     }
-    if (key[KEY_INPUT_RIGHT] == 1 || key[KEY_INPUT_D] == 1) {
-        // アーム本体を右に移動させる
-        armPosA.x += SPEED;
-        magnetPos.x += SPEED;
+    // キー"→"が押された場合 && アーム座標Xが画面の幅より小さい場合
+    if (key[KEY_INPUT_RIGHT] == 1 && vectorArmPos.x < WIDTH) {
+        vectorArmPos.x += SPEED_DEFAULT; // アーム本体を右に移動させる
+    }
+    // キー"W"が押された場合 && ワイヤー長が0より大きい場合
+    if (key[KEY_INPUT_W] == 1 && floatStaticWireLength > 0) {
+        floatStaticWireLength -= SPEED_WIRE; // アーム本体を上に移動させる
+    }
+    // キー"S"が押された場合 && ワイヤー長がワイヤー最大長 - ワイヤー速度より小さい場合
+    if (key[KEY_INPUT_S] == 1 && floatStaticWireLength < floatWireMaxLength - SPEED_DEFAULT) {
+        floatStaticWireLength += SPEED_WIRE; // アーム本体を下に移動させる
     }
 }
--- src/utils.h
+++ src/utils.h
@@ -12,20 +12,6 @@
 #include "vector.h"
 using namespace std;
 
-// 関数のプロトタイプ宣言
-int random(int min, int max);
-Vector2 addVector(Vector2 V1, Vector2 V2);
-Vector2 subVector(Vector2 V1, Vector2 V2);
-float crossProduct(Vector2 V1, Vector2 V2);
-float dotProduct(Vector2 V1, Vector2 V2);
-Vector2 relative(Vector2 V1, Vector2 V2);
-Vector2 normalized(Vector2 V1);
-float getDistance(Vector2 V1, Vector2 V2);
-bool pointBetween(Vector2 V1, Vector2 V2, Vector2 V3);
-Vector2 findDistance(Vector2 V1, Vector2 V2);
-Vector2 dotNearestNeighborSearch(Vector2 V1, Vector2 V2);
-void moveCross(Vector2 *pVec2, Vector2 vec2, int p);
-
 // ##### ランダムな値を返却する関数 #####
 // 参考文献 : http://vivi.dyndns.org/tech/cpp/random.html
 // 引数     : min, max
@@ -44,8 +30,8 @@ int random(int min, int max) {
 // 引数     : V1, V2
 // 戻り値   : 加算したベクトル
 // 注意     : なし
-Vector2 addVector(Vector2 V1, Vector2 V2) {
-    return Vector2(V1.x + V2.x, V1.y + V2.y);
+Vector2 addVector(const Vector2& V1, const Vector2& V2) {
+    return {V1.x + V2.x, V1.y + V2.y};
 };
 
 // ##### ベクトルの減算 #####
@@ -53,8 +39,17 @@ Vector2 addVector(Vector2 V1, Vector2 V2) {
 // 引数     : V1, V2
 // 戻り値   : 減算したベクトル
 // 注意     : なし
-Vector2 subVector(Vector2 V1, Vector2 V2) {
-    return Vector2(V1.x - V2.x, V1.y - V2.y);
+Vector2 subVector(const Vector2& V1, const Vector2& V2) {
+    return {V1.x - V2.x, V1.y - V2.y};
+};
+
+// ##### ベクトルの乗算 #####
+// 参考文献 : なし
+// 引数     : V1, V2
+// 戻り値   : 乗算したベクトル
+// 注意     : なし
+Vector2 mulVector(const Vector2& V1, const Vector2& V2) {
+    return {V1.x * V2.x, V1.y * V2.y};
 };
 
 // ##### 外積計算関数 #####
@@ -62,8 +57,8 @@ Vector2 subVector(Vector2 V1, Vector2 V2) {
 // 引数     : x1, y1, x2, y2
 // 戻り値   : 外積
 // 注意     : なし
-float crossProduct(Vector2 V1, Vector2 V2) {
-    return (float)V1.x * V2.y - V1.y * V2.x;
+float crossProduct(const Vector2& V1, const Vector2& V2) {
+    return V1.x * V2.y - V1.y * V2.x;
 }
 
 // ##### 内積計算関数 #####
@@ -71,8 +66,8 @@ float crossProduct(Vector2 V1, Vector2 V2) {
 // 引数     : Vector2 V1, Vector2 V2
 // 戻り値   : 内積
 // 注意     : なし
-float dotProduct(Vector2 V1, Vector2 V2) {
-    return (float)V1.x * V2.x + V1.y * V2.y;
+float dotProduct(const Vector2& V1, const Vector2& V2) {
+    return V1.x * V2.x + V1.y * V2.y;
 }
 
 // ##### 相対ベクター計算関数 #####
@@ -80,10 +75,35 @@ float dotProduct(Vector2 V1, Vector2 V2) {
 // 引数     : x1, y1, x2, y2
 // 戻り値   : 相対ベクター
 // 注意     : なし
-Vector2 relative(Vector2 V1, Vector2 V2) {
+Vector2 relative(const Vector2& V1, const Vector2& V2) {
     // 2つの円の中心座標の差分を計算（相対ベクトル）
     // 目的の地点 - 現在の地点
-    return Vector2(V1.x - V2.x, V1.y - V2.y);
+    return {V1.x - V1.y, V2.x - V2.y};
+};
+
+// ##### 2つの円の中心座標(Vector)の距離を計算する関数 #####
+// 参考文献 : https://www.geisya.or.jp/~mwm48961/math/distance1j.htm
+// 引数     : Vector2 V1, Vector2 V2
+// 戻り値   : 2つの円の中心座標の距離
+// 注意     : sqrt関数を使用するため、math.h or cmathをインクルードする必要がある
+float getVectorDistance(const Vector2& V1, const Vector2& V2) {
+    // 2つの円の中心座標の差分を計算（相対ベクトル）
+    // 目的の地点 - 現在の地点
+    Vector2 diff = subVector(V1, V2);
+    // 2つの円の中心座標の距離を計算
+    return sqrtf(powf(diff.x, 2) + powf(diff.y, 2));
+};
+
+// ##### 相対ベクターの長さを計算する関数 #####
+// 参考文献 : なし
+// 引数     : Vector2 V1
+// 戻り値   : 相対ベクターの長さ
+// 注意     : sqrt関数を使用するため、math.h or cmathをインクルードする必要がある
+float getVectorLength(const Vector2& V1) {
+    // 相対ベクトルの計算
+    Vector2 relativeVector = mulVector(V1, V1);
+    // 相対ベクトルの長さを計算
+    return sqrtf(relativeVector.x + relativeVector.y);
 };
 
 // ##### 相対ベクター正規化 #####
@@ -91,43 +111,28 @@ Vector2 relative(Vector2 V1, Vector2 V2) {
 // 引数     : V1
 // 戻り値   : 相対ベクター
 // 注意     : なし
-Vector2 normalized(Vector2 V1) {
-    // 相対ベクトルを正規化する
-    float length = getDistance(V1, V1);
+Vector2 normalized(const Vector2& V1) {
     // 正規化した相対ベクトルを返却する
-    return Vector2(V1.x / length, V1.y / length);
+    //return {V1.x / getVectorLength(V1), V1.y / getVectorLength(V1)};
+    return {
+        V1.x / sqrt((V1.x * V1.x) + (V1.y * V1.y)),
+        V1.y / sqrt((V1.x * V1.x) + (V1.y * V1.y))
+    };
 };
-
-// ##### 2つの円の中心座標の距離を計算する関数 #####
-// 参考文献 : https://www.geisya.or.jp/~mwm48961/math/distance1j.htm
-// 引数     : Vector2 V1, Vector2 V2
-// 戻り値   : 2つの円の中心座標の距離
-// 注意     : sqrt関数を使用するため、math.h or cmathをインクルードする必要がある
-float getDistance(Vector2 V1, Vector2 V2) {
-    // 2つの円の中心座標の差分を計算（相対ベクトル）
-    // 目的の地点 - 現在の地点
-    Vector2 diff = subVector(V1, V2);
-    // 2つの円の中心座標の距離を計算
-    return sqrt(pow(diff.x, 2) + pow(diff.y, 2));
-}
 
 // ##### 内積と外積を計算して点と点の間だったらtrueを返す #####
 // 参考文献 : なし
 // 引数     : Vector2 V1, Vector2 V2, Vector2 V3
 // 戻り値   : 点と点の間だったらtrue
 // 注意     : なし
-bool pointBetween(Vector2 V1, Vector2 V2, Vector2 V3) {
+bool pointBetween(const Vector2& V1, const Vector2& V2, const Vector2& V3) {
     // 点と点の間かどうかを判定する
     // 2つの円の中心座標の差分を計算（相対ベクトル）
     // 目的の地点 - 現在の地点
     Vector2 V4 = subVector(V2, V1);
     Vector2 V5 = subVector(V3, V1);
-    // 外積を計算
-    float crossProduct = crossProduct (V4, V5);
-    // 内積を計算
-    float dotProduct = dotProduct (V4, V5);
     // 点と点の間の幅前後100に侵入してきたらtrueを返す
-    if (crossProduct >= -10 || crossProduct <= 10 && dotProduct >= 0) {
+    if (dotProduct (V4, V5) >= -10 || crossProduct (V4, V5) <= 10 && dotProduct (V4, V5) >= 0) {
         return true;
     }
     // 内積と外積を計算して点と点の間だったらtrueを返す
@@ -135,18 +140,15 @@ bool pointBetween(Vector2 V1, Vector2 V2, Vector2 V3) {
 }
 
 // 判定処理を共通化
-Vector2 findDistance(Vector2 V1, Vector2 V2) {
+Vector2 findDistance(const Vector2& V1, const Vector2& V2) {
     // 相対ベクトルを正規化
     Vector2 relativeVector = {
-        (V1.x - V2.x) / getDistance(V1, V2),
-        (V1.y - V2.y) / getDistance(V1, V2)
+        (V1.x - V2.x) / getVectorDistance (V1, V2),
+        (V1.y - V2.y) / getVectorDistance (V1, V2)
     };
 
-    // return 時に入れ替えする方法がわからないのでPOWER IS POWER
-    Vector2 tmp = { relativeVector.y, -relativeVector.x };
-
     // 中央物体（球体）の進行方向ベクトルを返却
-    return tmp;
+    return { relativeVector.y, -relativeVector.x };
 };
 
 // ##### 最近傍点を探す #####
@@ -154,7 +156,7 @@ Vector2 findDistance(Vector2 V1, Vector2 V2) {
 // 引数     : V1 目的の地点, V2 現在の地点
 // 戻り値   : 最近傍点(Vector2)
 // 注意     : なし
-Vector2 dotNearestNeighborSearch(Vector2 V1, Vector2 V2) {
+Vector2 dotNearestNeighborSearch(const Vector2& V1, const Vector2& V2) {
     // 内積を計算する
     float dot = dotProduct (V1, V2);
     Vector2 V3 = normalized (V1);
@@ -169,7 +171,7 @@ Vector2 dotNearestNeighborSearch(Vector2 V1, Vector2 V2) {
 // 引数     : pPos, vec2, p
 // 戻り値   : なし
 // 注意     : pには1か2を入れる必要がある。1はpPos1、2はpPos2を操作する
-void moveCross(Vector2 *pVec2, Vector2 vec2, int p) {
+void moveCross(Vector2 *pVec2, const Vector2& vec2, int p) {
     if (p == 1) {
         // 十字キーでpPos2を移動させる
         if (CheckHitKey(KEY_INPUT_UP)) {
